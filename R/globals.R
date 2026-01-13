19e58f1dc102298a519754507061f8fe6c0f90de
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2026-01-13 15:42:50 +0100

    delete utils from imports, since it is not needed. added orcid ids to the contributors, and cleaned up the structure of the description file

--- R/globals.R
+++ R/globals.R
@@ -4,11 +4,6 @@
 utils::globalVariables(c(".row_id"))
 utils::globalVariables(c(".scrambled_rows"))
 
-# Ensure 'utils' is visibly used (avoids false "not imported" NOTE)
-# Use a condition that is not statically FALSE
-utils::capture.output(NULL)
-
-
 # ──────────────────────────────────────────────────────────────────────────────
 # Generic input validation helper functions
 # ──────────────────────────────────────────────────────────────────────────────
